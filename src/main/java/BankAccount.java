9deade0c09d806f94b115b52caf9cd3d910c0199
jshorns <joey.s.hornsby@gmail.com>
2021-07-21 12:39:07 +0100

    added method to initialize with empty transaction history and test for this

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,7 +1,22 @@
+import java.util.ArrayList;
+
 public class BankAccount {
     public float balance = 0;
+    ArrayList transactionHistory = new ArrayList<>();
 
     float getBalance() {
         return balance;
+    }
+
+    void deposit(float amount) {
+        balance += amount;
+    }
+
+    void withdrawal(float amount) {
+        balance -= amount;
+    }
+
+    ArrayList getTransactionHistory() {
+        return transactionHistory;
     }
 }
--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BankAccountTest {
@@ -8,5 +10,25 @@ class BankAccountTest {
     public void testGetBalance() {
         BankAccount bankAccount = new BankAccount();
         assertEquals(0, bankAccount.getBalance());
+    }
+    @Test
+    public void testGetTransactionHistory() {
+        BankAccount bankAccount = new BankAccount();
+        assertIterableEquals(new ArrayList<>(), bankAccount.getTransactionHistory());
+    }
+
+    @Test
+    public void testDeposit() {
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.deposit(400);
+        assertEquals(400, bankAccount.getBalance());
+    }
+
+    @Test
+    public void testWithdrawal() {
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.deposit(400);
+        bankAccount.withdrawal(200);
+        assertEquals(200, bankAccount.getBalance());
     }
 }
