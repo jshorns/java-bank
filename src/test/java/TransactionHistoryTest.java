7d4d623186d583ecb8b4f1ba20b9daa271b86f8b
jshorns <joey.s.hornsby@gmail.com>
2021-07-22 18:03:10 +0100

    introducing mocking - installed Mockito dependency and v basic mocking in transaction history test

--- src/test/java/TransactionHistoryTest.java
+++ src/test/java/TransactionHistoryTest.java
@@ -1,18 +1,25 @@
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.ArrayList;
 
+@ExtendWith(MockitoExtension.class)
 class TransactionHistoryTest {
+    @Mock
+    Transaction transaction;
+
     LocalDateTime testDate1 = LocalDateTime.of(2021, Month.JULY, 19, 19, 30, 40);
     LocalDateTime testDate2 = LocalDateTime.of(2021, Month.JULY, 20, 19, 30, 40);
     LocalDateTime testDate3 = LocalDateTime.of(2021, Month.JULY, 21, 19, 30, 40);
     Transaction transaction1 = new Transaction(300, true, 500, testDate1);
     Transaction transaction2 = new Transaction(300, true, 800, testDate2);
     Transaction transaction3 = new Transaction(300, true, 1200, testDate3);
-
 
     @Test
     public void testGetTransactions(){
@@ -22,7 +29,7 @@ class TransactionHistoryTest {
 
     @Test
     public void testAddTransaction(){
-        Transaction transaction = new Transaction(0, true, 0);
+
         TransactionHistory transactionHistory = new TransactionHistory();
         transactionHistory.addTransaction(transaction);
         assertEquals(1, transactionHistory.getTransactions().size());
@@ -31,7 +38,6 @@ class TransactionHistoryTest {
 
     @Test
     public void getLastTransaction(){
-        Transaction transaction = new Transaction(0, true, 0);
         TransactionHistory transactionHistory = new TransactionHistory();
         transactionHistory.addTransaction(transaction);
         assertEquals(transaction, transactionHistory.getLastTransaction());
